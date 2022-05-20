9f9970feb996ea34b5c97727c26c069e1c9f0637
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2022-05-21 02:43:13 +0700

    feat: inisiasi card sales

--- src/components/card/sales/SalesCard.jsx
+++ src/components/card/sales/SalesCard.jsx
@@ -4,8 +4,15 @@ import React from "react";
 export default function Sales(){
   return(
     <>
-      <div>
-
+      <div className="flex p-14 w-64 bg-white">
+        <div className="flex flex-row justify-around">
+          <div>
+            <p>Sales Turnover</p>
+          </div>
+          <div>
+            <p>buat image</p>
+          </div>
+        </div>
       </div>
     </>
   );
