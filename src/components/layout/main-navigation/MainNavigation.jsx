a467ab831547568123cb82248a2d62ff7fdd7f74
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2022-05-20 13:44:49 +0700

    feat: adding main navigation

--- src/components/layout/main-navigation/MainNavigation.jsx
+++ src/components/layout/main-navigation/MainNavigation.jsx
@@ -1,1 +1,28 @@
-export default function MainNavigation(){}
+import React from "react";
+
+import ProfilePicture from '../../assets/Profile.png';
+import Logout from '../../assets/logout.png'
+
+
+export default function MainNavigation(){
+    return(
+        <div className="px-10 bg-white shadow-md sticky top-0 h-16 w-screen p-4 flex items-center justify-between">
+            <div>
+                <p className="text-4xl">Advotics</p>
+            </div>
+            <div className="flex items-center">
+                <div className="flex-col justify-center px-3">
+                    <p className="text-center">Username</p>
+                    <p className="text-center">Company Name</p>
+                </div>
+                <div className="px-3">
+                    <img src={ProfilePicture} alt="profile picture"/>
+                </div>
+                <div className="px-3">
+                    <img src={Logout} alt="profile picture"/>
+                </div>
+            </div>
+        </div>
+
+    );
+}
